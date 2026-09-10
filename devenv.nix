f08256d1467a692b6685de930c672bc98d2298a3
Liam Gingrich <jlgingrich@proton.me>
2026-09-10 21:39:48 +0000

    Add cargo automation

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,44 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/languages/
-  languages.rust.enable = true;
-
   # See full reference at https://devenv.sh/reference/options/
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rust-analyzer"
+    ];
+  };
+
+  packages = with pkgs; [
+    bacon
+    cargo-seek
+    cargo-nextest
+    cargo-generate
+  ];
+  scripts.watcher = {
+    exec = ''
+      watchexec -c -e rs \
+      "cargo clippy && cargo test && cargo run"
+    '';
+    packages = [ pkgs.watchexec ];
+  };
+
+  enterShell = ''
+    echo "Crates ready to update with 'cargo update'":
+    cargo update -n
+  '';
+  git-hooks.hooks = {
+    clippy.enable = true;
+  };
+
 }
